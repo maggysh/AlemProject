e8df0dea289a376028788d467f2c6bd8126f8b63
Amina Fazlija <amina.fazlija13@gmail.com>
2018-02-13 12:34:28 +0100

    datum

--- AlemProject/App/client/app/user/izvjestaj/izvjestaj.controller.js
+++ AlemProject/App/client/app/user/izvjestaj/izvjestaj.controller.js
@@ -10,75 +10,81 @@ angular.module('appApp')
     $scope.displayed = [];
     $scope.distinctTipSenzora = [];
     $scope.distinctDatum = [];
+    $scope.pocetniDatum='';
+    $scope.krajnjiDatum='';
     //---------------------------------------------------------------------------------------------
     $scope.Init = function(){
-        $http.get('/loggedin').then(function (response) {
-                  //$scope.info.name = response.data.user.ime_prezime;
-                  $scope.info.id = response.data.user.id;
-                  $http.get('/api/user/link/' + $scope.info.id).then(function(data){
-                    $scope.pregled.stanice = data.data;
-                  });
-         });
+      $http.get('/loggedin').then(function (response) {
+        //$scope.info.name = response.data.user.ime_prezime;
+        $scope.info.id = response.data.user.id;
+        $http.get('/api/user/link/' + $scope.info.id).then(function(data){
+          $scope.pregled.stanice = data.data;
+        });
+      });
     };
     $scope.Init();
     //---------------------------------------------------------------------------------------------
     //Uzimanje svih tipova senzora
     $scope.tipoviSenzora = function(){
-        $scope.distinctTipSenzora = [];
-        $scope.distinctDatum = [];
-        $scope.distinctDatum.datumi = [];
-        $scope.temp=[];
-        $scope.distinctDatum.vrijednosti = [];
-        var i = 0;
-        angular.forEach($scope.collection, function(item) {
-             var value = item.Tip_senzora;
-             //var date =$filter('date')(new Date(item.Datum), "dd/MM/yyyy");
-             var date = item.Datum;
+      $scope.distinctTipSenzora = [];
+      $scope.distinctDatum = [];
+      $scope.distinctDatum.datumi = [];
+      $scope.temp=[];
+      $scope.distinctDatum.vrijednosti = [];
+      var i = 0;
+      angular.forEach($scope.collection, function(item) {
+        var value = item.Tip_senzora;
+        //var date =$filter('date')(new Date(item.Datum), "dd/MM/yyyy");
+        var date = item.Datum;
 
-             if (value && value.trim().length > 0 && $scope.distinctTipSenzora.indexOf(value) === -1) {
-                  $scope.distinctTipSenzora.push(value);
-             };
-             if (date && date.trim().length > 0 && $scope.temp.indexOf(date) === -1) {
-                   $scope.temp.push(date);
-                   $scope.distinctDatum.datumi[i] = [];
-                   $scope.distinctDatum.datumi[i].push(date);
-                   i++;
-             };
-        });
+        if (value && value.trim().length > 0 && $scope.distinctTipSenzora.indexOf(value) === -1) {
+          $scope.distinctTipSenzora.push(value);
+        };
+        if (date && date.trim().length > 0 && $scope.temp.indexOf(date) === -1) {
+          $scope.temp.push(date);
+          $scope.distinctDatum.datumi[i] = [];
+          $scope.distinctDatum.datumi[i].push(date);
+          i++;
+        };
+      });
 
-        angular.forEach($scope.collection, function(item, key) {
-            var k = 0;
-            var t = -1;
-            while(t == -1){
-                t = $scope.distinctDatum.datumi[k].indexOf(item.Datum);
-                k++;
-            }
-            var indeks_tipSenzora = $scope.distinctTipSenzora.indexOf(item.Tip_senzora);
-            $scope.distinctDatum.datumi[k-1][indeks_tipSenzora+1] = item.Vrijednost;
-        });
-        $scope.collection = $scope.distinctDatum.datumi;
-        $scope.displayed = [].concat($scope.collection);
+      angular.forEach($scope.collection, function(item, key) {
+        var k = 0;
+        var t = -1;
+        while(t == -1){
+          t = $scope.distinctDatum.datumi[k].indexOf(item.Datum);
+          k++;
+        }
+        var indeks_tipSenzora = $scope.distinctTipSenzora.indexOf(item.Tip_senzora);
+        $scope.distinctDatum.datumi[k-1][indeks_tipSenzora+1] = item.Vrijednost;
+      });
+      $scope.collection = $scope.distinctDatum.datumi;
+      $scope.displayed = [].concat($scope.collection);
     };
     //---------------------------------------------------------------------------------------------
     $scope.Osvjezi = function(){
-        $scope.collection = [];
-        $scope.displayed = [];
-        $http.get('/api/user/izvjestaj/' + $scope.pregled.id).then(function (data) {
-                   angular.forEach(data.data, function(value) {
-                       angular.forEach(value.vrijednosti, function(entry, key){
-                            $scope.collection.push({
-                                 "Vrijednost": entry.Vrijednost,
-                                 "Datum": $filter('date')(entry.Datum, "dd/MM/yyyy")+ ' ' + $filter('date')(entry.Datum, "HH:mm:ss"),
-                                 "Kod_senzora": value.senzor.Kod_senzora,
-                                 "Tip_senzora": value.senzor.Tip_senzora.Tip_Senzora,
-                             });
-                        });
+      $scope.collection = [];
+      $scope.displayed = [];
+      $http.get('/api/user/izvjestaj/' + $scope.pregled.id).then(function (data) {
+        angular.forEach(data.data, function(value) {
+          angular.forEach(value.vrijednosti, function(entry, key){
+            var time = '';
+            time = new Date(entry.Datum);
+            if(time>$scope.pocetniDatum && time<$scope.krajnjiDatum) {
+              time = time.getTime();
+              $scope.collection.push({
+                "Vrijednost": entry.Vrijednost,
+                "Datum": $filter('date')(entry.Datum, "dd/MM/yyyy"+ ' ' + $filter('date')(entry.Datum, "HH:mm:ss")),
+                "Kod_senzora": value.senzor.Kod_senzora,
+                "Tip_senzora": value.senzor.Tip_senzora.Tip_Senzora,
+              });
+            }
+          });
+        });
+        $scope.tipoviSenzora();
+      });
 
-                   });
-                   $scope.tipoviSenzora();
-                });
-
-        $scope.displayed = [].concat($scope.collection);
+      $scope.displayed = [].concat($scope.collection);
     };
     //---------------------------------------------------------------------------------------------
     $scope.printIt = function(){
@@ -109,169 +115,148 @@ angular.module('appApp')
 
     };
 
-   $scope.printIt2 = function(){
-     console.log("scope ",$scope.collection);
+    $scope.printIt2 = function(){
+      console.log("scope ",$scope.collection);
       var data = [];
-      //var data = $scope.collection.slice();
-     // var data =$scope.collection.join(';');
       console.log("data",data);
       var temp={};
       console.log("scople colleciton",$scope.collection);
       var csvTemp={};
-     var  csvContent= 'data:text/csv;charset=utf-8,';
-     angular.forEach($scope.collection, function(entry, key){
-     angular.forEach(entry, function(value, keyy){
-       //console.log(value);
-
-        //if(keyy == 0){
-        //  temp["Datum"] = value;
-      //  }
-      //  else {
-         // temp[$scope.distinctTipSenzora[keyy - 1]] = value;
+      var  csvContent= 'data:text/csv;charset=utf-8,';
+      angular.forEach($scope.collection, function(entry, key){
+        angular.forEach(entry, function(value, keyy){
           if(keyy < entry.length ){
-          csvContent +=  entry +','+'\r\n';//: value +';'+'\r\n';
-        }//}
-     });
-   });
-     //csvContent += value + "\r\n"; // add carriage return
-          var encodedUri = encodeURI(csvContent);
-          var link = document.createElement("a");
-          link.setAttribute("href", encodedUri);
-          link.setAttribute("download", "my_data.csv");
-         // document.body.appendChild(link); // Required for FF
-
-          link.click();
-
-
-
-      //  var temp = {};
-};   //
-            //temp[$scope.distinctTipSenzora[keyy-1]]=value;
-        //    csvTemp=$scope.convertArrayOfObjectsToCSV();
-            console.log("csv");
-         // };
-       // });
-        //data.push(csvTemp);
-
-
-/*$scope.convertArrayOfObjectsToCSV=function() {
-  var result, ctr, keys, data;
-  var columnDelimiter=',';
-  var lineDelimiter='\n';
-  angular.forEach($scope.collection, function (entry, key) {
-    console.log("prvi");
-    angular.forEach(entry, function (value, keyy) {
-      console.log("drugi");
-    var temp = {};
-    if (keyy == 0) {
-      temp["Datum"] = value;
-    }
-    else {
-        temp[$scope.distinctTipSenzora[keyy - 1]] = value;
-      console.log("treci");
-     //   columnDelimiter = args.columnDelimiter || ',';
-     //   lineDelimiter = args.lineDelimiter || '\n';
-        //keys = Object.keys(data[0]);
-      foreach(temp, function(value,key))
-      {
-        console.log(temp);
-      }
-
-       /* result = '';
-        result += keys.join(columnDelimiter);
-        console.log("cetvrti");
-        result += lineDelimiter;
-        temp.forEach(function (item) {
-          ctr = 0;
-          keys.forEach(function (key) {
-            if (ctr > 0) result += columnDelimiter;
-
-            result += item[key];
-            ctr++;
-          });
-          result += lineDelimiter;
+            csvContent +=  entry +','+'\r\n';
+          }
         });
+      });
+      var encodedUri = encodeURI(csvContent);
+      var link = document.createElement("a");
+      link.setAttribute("href", encodedUri);
+      link.setAttribute("download", "my_data.csv");
+      link.click();
+    };
 
-        return result;
 
+
+    /*$scope.convertArrayOfObjectsToCSV=function() {
+      var result, ctr, keys, data;
+      var columnDelimiter=',';
+      var lineDelimiter='\n';
+      angular.forEach($scope.collection, function (entry, key) {
+        console.log("prvi");
+        angular.forEach(entry, function (value, keyy) {
+          console.log("drugi");
+        var temp = {};
+        if (keyy == 0) {
+          temp["Datum"] = value;
+        }
+        else {
+            temp[$scope.distinctTipSenzora[keyy - 1]] = value;
+          console.log("treci");
+         //   columnDelimiter = args.columnDelimiter || ',';
+         //   lineDelimiter = args.lineDelimiter || '\n';
+            //keys = Object.keys(data[0]);
+          foreach(temp, function(value,key))
+          {
+            console.log(temp);
+          }
+
+           /* result = '';
+            result += keys.join(columnDelimiter);
+            console.log("cetvrti");
+            result += lineDelimiter;
+            temp.forEach(function (item) {
+              ctr = 0;
+              keys.forEach(function (key) {
+                if (ctr > 0) result += columnDelimiter;
+
+                result += item[key];
+                ctr++;
+              });
+              result += lineDelimiter;
+            });
+
+            return result;
+
+          }
+        });
       }
-    });
-  }
-};*/
+    };*/
     //---------------------------------------------------------------------------------------------
     //DATE TIME PICKER
     $scope.today = function() {
-          $scope.dt = new Date();
-        };
-        $scope.today();
+      $scope.dt = new Date();
+    };
+    $scope.today();
 
-        $scope.clear = function () {
-          $scope.dt = null;
-        };
+    $scope.clear = function () {
+      $scope.dt = null;
+    };
 
-        $scope.toggleMin = function() {
-          $scope.minDate = $scope.minDate ? null : new Date();
-        };
-        $scope.toggleMin();
-        $scope.maxDate = new Date(2020, 5, 22);
+    $scope.toggleMin = function() {
+      $scope.minDate = $scope.minDate ? null : new Date();
+    };
+    $scope.toggleMin();
+    $scope.maxDate = new Date(2020, 5, 22);
 
-        $scope.open_prije = function($event) {
-          $scope.status_prije.opened = true;
-        };
-         $scope.open_poslije = function($event) {
-           $scope.status_poslije.opened = true;
-        };
+    $scope.open_prije = function($event) {
+      $scope.status_prije.opened = true;
+    };
+    $scope.open_poslije = function($event) {
+      $scope.status_poslije.opened = true;
+    };
 
-        $scope.setDate = function(year, month, day) {
-          $scope.dt = new Date(year, month, day);
-        };
+    $scope.setDate = function(year, month, day) {
+      $scope.dt = new Date(year, month, day);
+    };
 
-        $scope.dateOptions = {
-          formatYear: 'yy',
-          startingDay: 1
-        };
+    $scope.dateOptions = {
+      formatYear: 'yy',
+      startingDay: 1
+    };
 
-        $scope.formats = ['yyyy-MM-dd'];
-        $scope.format = $scope.formats[0];
+    $scope.formats = ['yyyy-MM-dd'];
+    $scope.format = $scope.formats[0];
 
-        $scope.status_prije = {
-          opened: false
-        };
-        $scope.status_poslije = {
-          opened: false
-        };
+    $scope.status_prije = {
+      opened: false
+    };
+    $scope.status_poslije = {
+      opened: false
+    };
 
-        var tomorrow = new Date();
-        tomorrow.setDate(tomorrow.getDate() + 1);
-        var afterTomorrow = new Date();
-        afterTomorrow.setDate(tomorrow.getDate() + 2);
-        $scope.events =
-          [
-            {
-              date: tomorrow,
-              status: 'full'
-            },
-            {
-              date: afterTomorrow,
-              status: 'partially'
-            }
-          ];
+    var tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    var afterTomorrow = new Date();
+    afterTomorrow.setDate(tomorrow.getDate() + 2);
+    $scope.events =
+      [
+        {
+          date: tomorrow,
+          status: 'full'
+        },
+        {
+          date: afterTomorrow,
+          status: 'partially'
+        }
+      ];
 
-        $scope.getDayClass = function(date, mode) {
-          if (mode === 'day') {
-            var dayToCheck = new Date(date).setHours(0,0,0,0);
+    $scope.getDayClass = function(date, mode) {
+      if (mode === 'day') {
+        var dayToCheck = new Date(date).setHours(0,0,0,0);
 
-            for (var i=0;i<$scope.events.length;i++){
-              var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+        for (var i=0;i<$scope.events.length;i++){
+          var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
 
-              if (dayToCheck === currentDay) {
-                return $scope.events[i].status;
-              }
-            }
+          if (dayToCheck === currentDay) {
+            return $scope.events[i].status;
           }
+        }
+      }
 
-          return '';
-        };
+      return '';
+    };
     //---------------------------------------------------------------------------------------------
 
   });
