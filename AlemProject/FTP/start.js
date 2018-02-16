bb02ee74aedce173bf131cdb41e2855b69a3da7a
LejlaBrgulja1 <lbrgulja1@etf.unsa.ba>
2018-02-16 14:53:52 +0100

    try catch u start.js

--- AlemProject/FTP/start.js
+++ AlemProject/FTP/start.js
@@ -47,98 +47,102 @@ function doMagic(fileName){
 
         function(arg1, callback){
             console.log("Čitanje file-a i upisivanje vrijednosti u bazu");
-            async.forEachOf(arg1.split('<STATION>'), function(podatak, key,callback){
-                console.log(podatak);
-                var stationID = '';
-                var senzorID = '';
-                var godina = '';
-                var mjesec = '';
-                var dan = '';
-                var sati = '';
-                var minute = '';
-                var sekunde = '';
-                var vrijednost = '';
-                if (podatak != ''){
-                    stationID = podatak.substring(0, 10);
-
-                    podatak = podatak.replace(stationID + "</STATION><SENSOR>", "");
+            try{
+                async.forEachOf(arg1.split('<STATION>'), function(podatak, key,callback){
                     console.log(podatak);
-                    senzorID = podatak.substring(0, 4);
-                    console.log(senzorID);
-                    podatak = podatak.replace(/(?:\r\n|\r|\n)/g, '<br>');
-                    podatak = podatak.replace(senzorID + "</SENSOR><DATEFORMAT>YYYYMMDD</DATEFORMAT>" + "<br>", "");
-                    godina = podatak.substring(0, 4);
-                    console.log(godina);
-                    podatak = podatak.replace(godina, "");
-                    mjesec = podatak.substring(0, 2);
-                    podatak = podatak.replace(mjesec, "");
-                    dan = podatak.substring(0, 2);
-                    podatak = podatak.replace(dan + ";", "");
-                    sati = podatak.substring(0, 2);
-                    minute = podatak.substring(2, 4);
-                    sekunde = podatak.substring(4, 6);
-                    i = 7;
-                    podatak = podatak.replace("<br>", ",");
-                    while(podatak[i] != ',') {
-                        vrijednost = vrijednost + podatak[i];
-                        console.log("vrijednost ----",vrijednost);
-                        i++;
-                    }
+                    var stationID = '';
+                    var senzorID = '';
+                    var godina = '';
+                    var mjesec = '';
+                    var dan = '';
+                    var sati = '';
+                    var minute = '';
+                    var sekunde = '';
+                    var vrijednost = '';
+                    if (podatak != ''){
+                        stationID = podatak.substring(0, 10);
 
-                    console.log('stationID', stationID);
+                        podatak = podatak.replace(stationID + "</STATION><SENSOR>", "");
+                        console.log(podatak);
+                        senzorID = podatak.substring(0, 4);
+                        console.log(senzorID);
+                        podatak = podatak.replace(/(?:\r\n|\r|\n)/g, '<br>');
+                        podatak = podatak.replace(senzorID + "</SENSOR><DATEFORMAT>YYYYMMDD</DATEFORMAT>" + "<br>", "");
+                        godina = podatak.substring(0, 4);
+                        console.log(godina);
+                        podatak = podatak.replace(godina, "");
+                        mjesec = podatak.substring(0, 2);
+                        podatak = podatak.replace(mjesec, "");
+                        dan = podatak.substring(0, 2);
+                        podatak = podatak.replace(dan + ";", "");
+                        sati = podatak.substring(0, 2);
+                        minute = podatak.substring(2, 4);
+                        sekunde = podatak.substring(4, 6);
+                        i = 7;
+                        podatak = podatak.replace("<br>", ",");
+                        while(podatak[i] != ',') {
+                            vrijednost = vrijednost + podatak[i];
+                            console.log("vrijednost ----",vrijednost);
+                            i++;
+                        }
 
-                    models.stanica.find({where: {Kod_stanice: stationID}}).then(function(stanica){
-                            if(stanica == null) return;
-                            console.log("VJEVERICA",stanica);
-                            models.senzor.find({where: {Kod_senzora: senzorID, StanicaId: stanica.dataValues.id}}).then(function(senzor){
-                                if(senzor == null) return;
-                                console.log(senzor);
-                                models.vrijednost.sync().then(function(){
-                                    models.vrijednost.create({
-                                        SenzorId: senzor.dataValues.id,
-                                        Datum: new Date(godina, mjesec, dan, sati, minute, sekunde),
-                                        Vrijednost: vrijednost
-                                    });
-                                }).then(function(){
-                                    //Slanje maila u slučaju prekoračenja vrijednosti na određenom senzoru
-                                    //Potrebno dodati i slučaj za prekoračenej gornje granice
-                                    models.user_senzor.find({where: {SenzorId: senzor.dataValues.id, Max: {lte: vrijednost}}}).then(function(data){
-                                        if(data != null){
-                                            console.log("data",data);
-                                            models.user.find({where: {id: data.dataValues.UserId}}).then(function(value){
-                                                if(value != null){
-                                                    setTimeout(function() {
-                                                        var txt = "Stanica: " + stationID + ", senzor: " + senzorID + ", vrijednost " + vrijednost;
-                                                        var mail = {
-                                                            from: "node proba <alemsistem365@gmail.com>",
-                                                            to: value.dataValues.email,
-                                                            subject: "Prekoracenje vrijednosti",
-                                                            text: txt,
-                                                            html: txt
-                                                        };
-                                                        smtpTransport.sendMail(mail, function(error, response){
-                                                            if(error){
-                                                                console.log(error);
-                                                            }else{
-                                                                console.log("Message sent: " + response.message);
-                                                            }
-                                                            smtpTransport.close();
-                                                        });
-                                                    }, 1000);
-                                                }
-                                            })
-                                        };
+                        console.log('stationID', stationID);
 
-                                    });
+                        models.stanica.find({where: {Kod_stanice: stationID}}).then(function(stanica){
+                                if(stanica == null) return;
+                                console.log("VJEVERICA",stanica);
+                                models.senzor.find({where: {Kod_senzora: senzorID, StanicaId: stanica.dataValues.id}}).then(function(senzor){
+                                    if(senzor == null) return;
+                                    console.log(senzor);
+                                    models.vrijednost.sync().then(function(){
+                                        models.vrijednost.create({
+                                            SenzorId: senzor.dataValues.id,
+                                            Datum: new Date(Date.UTC(godina, mjesec-1, dan, sati, minute, sekunde)),
+                                            Vrijednost: vrijednost
+                                        });
+                                    }).then(function(){
+                                        //Slanje maila u slučaju prekoračenja vrijednosti na određenom senzoru
+                                        //Potrebno dodati i slučaj za prekoračenej gornje granice
+                                        models.user_senzor.find({where: {SenzorId: senzor.dataValues.id, Max: {lte: vrijednost}}}).then(function(data){
+                                            if(data != null){
+                                                console.log("data",data);
+                                                models.user.find({where: {id: data.dataValues.UserId}}).then(function(value){
+                                                    if(value != null){
+                                                        setTimeout(function() {
+                                                            var txt = "Stanica: " + stationID + ", senzor: " + senzorID + ", vrijednost " + vrijednost;
+                                                            var mail = {
+                                                                from: "node proba <alemsistem365@gmail.com>",
+                                                                to: value.dataValues.email,
+                                                                subject: "Prekoracenje vrijednosti",
+                                                                text: txt,
+                                                                html: txt
+                                                            };
+                                                            smtpTransport.sendMail(mail, function(error, response){
+                                                                if(error){
+                                                                    console.log(error);
+                                                                }else{
+                                                                    console.log("Message sent: " + response.message);
+                                                                }
+                                                                smtpTransport.close();
+                                                            });
+                                                        }, 1000);
+                                                    }
+                                                })
+                                            };
+
+                                        });
+                                    })
                                 })
-                            })
-                        });
+                            });
 
+                        callback(null);
+                    }
+                }, function(err){
                     callback(null);
-                }
-            }, function(err){
-                callback(null);
-            });
+                });
+            }catch(err){
+                
+            }
         },
         // ------------------------------------------------------------------------------------------------------------------------------------
         // KRAJ WATERFALL FUNCKIJE
